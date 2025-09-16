d67587ad8bf6f7ba55e773bc9f3314448c0b925c
sergi <sergi.qoniashvili@gmail.com>
2025-09-16 16:58:42 +0200

    xz
    zx

---
+++ beispielfragen-pi2-klausur-main/beispielfragen-pi2-klausur-main/Aufgaben-Datenstrukturen/src/de/MainVorTest.java
@@ -0,0 +1,10 @@
+package de;
+
+public class MainVorTest {
+    public static void main(String[] args) {
+        Node<Integer> integerNode = new Node<>(2,null);
+
+        integerNode.getValue();
+
+    }
+}
--- src/klausurVobereitung/HMaps.java
+++ src/klausurVobereitung/HMaps.java
@@ -11,12 +11,47 @@ import java.util.Map;
 public class HMaps {
     public static void main(String[] args) {
 
+        //Hashmap hesrtellen
+        HashMap<String,Double> kokteilPreisZuordnung = new HashMap<>();
+        kokteilPreisZuordnung.put("RadMary", 10.10);
+        kokteilPreisZuordnung.put("wisky", 5.50);
+        kokteilPreisZuordnung.put("ColaWithWisky", 7.0);
+        kokteilPreisZuordnung.put("wodaSoda", 4.0);
+
+
+
+
+        //მთლიანი მონაცემების გამოტანა
+        System.out.println(kokteilPreisZuordnung);
+
+        //გასაღებით გამოტანა
+        System.out.println("Wisky's Preis ist  --->"  + kokteilPreisZuordnung.get("wisky"));
+
+        //მონაცემის არსებობის გასაღებით შემოწმება
+        System.out.println("prüft es mit Kay ob -wodaSoda-in Hashmap gibt. Ergebnis ---> "+ kokteilPreisZuordnung.containsKey("wodaSoda"));
+        //იმპლიმენტირება და გამოტანა
+
+        System.out.println("\n-------------იმპლიმენტირება და გამოტანა----------\n");
+
+        for(Map.Entry<String,Double> entry : kokteilPreisZuordnung.entrySet()){
+            System.out.println("Das Kokteil " + entry.getKey() + "  Preis ist " + entry.getValue());
+        }
+
+        // მონაცემის განახლება
+        System.out.println("\n------ მონაცემის განახლება --------\n");
+
+        kokteilPreisZuordnung.replace("wisky",11.0);
+        System.out.println(kokteilPreisZuordnung);
+
+
         HashMap<String,Double> essenPreisZuordnung = new HashMap<>();
         essenPreisZuordnung.put("Piza Funghi", 9.50);
         essenPreisZuordnung.put("Pommes", 8.50);
         essenPreisZuordnung.put("Döner", 9.90);
+
         essenPreisZuordnung.put("Margarita", 11.50);
 
+        /*
         //მთლიანი მონაცემების გამოტანა
         System.out.println(essenPreisZuordnung);
         // გასაღებით მონაცემის გამოტანა
@@ -34,6 +69,8 @@ public class HMaps {
         // მონაცემის განახლება
         essenPreisZuordnung.replace("Döner",7.00);
         System.out.println(essenPreisZuordnung);
+
+         */
 }
 
 
